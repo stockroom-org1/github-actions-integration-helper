fcd023b99fcce30dddc3021b8434b9b02861f0e3
Narayanan Chandran <nchandran@veracode.com>
2026-09-16 16:23:58 +0530

    update with 103

--- dist/index.js
+++ dist/index.js
@@ -85284,8 +85284,8 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const appConfig = {
     hostName: {
         veracode: {
-            us: 'api.veracode.com',
-            eu: 'api.veracode.eu'
+            us: 'api-agora-stage-103.stage.veracode.io',
+            eu: 'api-agora-stage-103.stage.veracode.io'
         },
         github: 'api.github.com'
     },
